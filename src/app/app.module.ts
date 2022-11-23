7df39eb9a30e69958ec6049384a25dd02f6e2f8b
Tenembris <110739948+Tenembris@users.noreply.github.com>
2022-11-23 14:27:44 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IntroComponent } from './intro/intro.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
